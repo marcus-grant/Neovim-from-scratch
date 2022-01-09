4d973dc1aba1714d8ee15811c1c82e58dbe9e6ee
Marcus Grant <marcusfg@protonmail.com>
2022-01-09 20:02:48 +0100

    Env: Notes about repo fork in init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
+-- My fork of https://github.com/LunarVim/Neovim-from-scratch
+-- Meant to eventually become my completely rewritten neovim config
+-- Took too long to rewrite everything myself while learning lua + neovim v0.6
+-- Will be merged into my https://github.com/marcus-grant/dots-neovim
+-- Forked by marcus-grant to marcus-grant/Neovim-from-scratch
+-- forked at 2022-01-09
+
+-- The list of in-order lazy loaded modules
 require "user.options"
 require "user.keymaps"
 require "user.plugins"
